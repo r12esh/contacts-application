0407c41da48dcb9ddd3357ceb340814c94c6da93
Ritesh Gupta <ritesh098765432@gmail.com>
2021-09-17 20:58:09 +0530

    Final commit

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -1,4 +1,10 @@
 const firebaseConfig = {
-  // your config
+  apiKey: "AIzaSyCY531IIXZSrgLUw_uoiKsO8g0rYomI8vQ",
+  authDomain: "codesandbox-practice-project.firebaseapp.com",
+  databaseURL: "https://codesandbox-practice-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "codesandbox-practice-project",
+  storageBucket: "codesandbox-practice-project.appspot.com",
+  messagingSenderId: "108106237794",
+  appId: "1:108106237794:web:516d97bf5bbf61b909ce3b"
 };
 export default firebaseConfig;
